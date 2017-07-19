6521cd439e6e37f23dd17fe6145cc5c89a627e9f
cedi <cedi@users.noreply.github.com>
2017-07-19 08:57:19 +0200

    [Set1 - Challenge 1] restructuring files

--- Set1/Challenge1/src/src/Base64.cpp
+++ Set1/Challenge1/src/src/Base64.cpp
@@ -13,7 +13,7 @@ const char Base64::reverseLookupTable[128] =
 {
    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-   64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+     64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
    64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
@@ -23,7 +23,7 @@ const char Base64::reverseLookupTable[128] =
 
 string Base64::encode(const string &bindata)
 {
-  if (bindata.size() > (numeric_limits<string::size_type>::max() / 4u) * 3u)
+  if(bindata.size() > (numeric_limits<string::size_type>::max() / 4u) * 3u)
   {
      throw length_error("Converting too large a string to base64.");
   }
@@ -37,11 +37,11 @@ string Base64::encode(const string &bindata)
   unsigned int accumulator = 0;
   const string::const_iterator binend = bindata.end();
 
-  for (string::const_iterator i = bindata.begin(); i != binend; ++i) 
+  for(string::const_iterator i = bindata.begin(); i != binend; ++i) 
   {
     accumulator = (accumulator << 8) | (*i & 0xffu);
     bits_collected += 8;
-    while (bits_collected >= 6) 
+    while(bits_collected >= 6) 
      {
        bits_collected -= 6;
        retval[outpos++] = lookupTable[(accumulator >> bits_collected) & 0x3fu];
@@ -49,7 +49,7 @@ string Base64::encode(const string &bindata)
   }
   
   // Any trailing bits that are missing.
-  if (bits_collected > 0)
+  if(bits_collected > 0)
   {
     accumulator <<= 6 - bits_collected;
     retval[outpos++] = lookupTable[accumulator & 0x3fu];
@@ -60,7 +60,6 @@ string Base64::encode(const string &bindata)
 
 string Base64::decode(const string &ascdata)
 {
-  using string;
   string retval;
   const string::const_iterator last = ascdata.end();
   int bits_collected = 0;
@@ -71,19 +70,19 @@ string Base64::decode(const string &ascdata)
     const int c = *i;
     
     // Skip whitespace and padding. Be liberal in what you accept.
-    if (isspace(c) || c == '=')
+    if(isspace(c) || c == '=')
     {
       continue;
     }
 
-    if ((c > 127) || (c < 0) || (reverseLookupTable[c] > 63))
+    if((c > 127) || (c < 0) || (reverseLookupTable[c] > 63))
     {
       throw invalid_argument("This contains characters not legal in a base64 encoded string.");
     }
 
     accumulator = (accumulator << 6) | reverseLookupTable[c];
     bits_collected += 6;
-    if (bits_collected >= 8)
+    if(bits_collected >= 8)
     {
       bits_collected -= 8;
       retval += (char)((accumulator >> bits_collected) & 0xffu);
