3ad01ca5cea20259806d2298034d6fe4aaff22b6
Cedric Kienzler <cedric.kienzler@sophos.com>
2017-07-20 08:58:10 +0200

    [Bugfix 1] fixed formating issues

--- Set1/Challenge1/src/include/Base64.hpp
+++ Set1/Challenge1/src/include/Base64.hpp
@@ -2,16 +2,16 @@
 
 #include <string>
 
-  class Base64
-  {
-  private:
-    static const char lookupTable[65];
-    static const char reverseLookupTable[128];
+class Base64
+{
+private:
+  static const char lookupTable[65];
+  static const char reverseLookupTable[128];
+  
+  // Private constructor to prevent instantiation of the class
+  Base64() {}
 
-    // Private constructor to prevent instantiation of the class
-    Base64() {}
-
-  public:
-    static std::string encode(const std::string &bindata);
-    static std::string decode(const std::string &ascdata);
+public:
+  static std::string encode(const std::string &bindata);
+  static std::string decode(const std::string &ascdata);
 };
--- Set1/Challenge2/src/src/Hexadecimal.cpp
+++ Set1/Challenge2/src/src/Hexadecimal.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <cstdint>
 
-
 #include "Hexadecimal.hpp"
 
 using namespace std;
@@ -13,38 +12,37 @@ const string Hexadecimal::table = "0123456789ABCDEF";
 
 string Hexadecimal::encode(const string& input)
 {
-	stringstream ss;
-
-	ss << hex << setfill('0');
+  stringstream ss;
+  
+  ss << hex << setfill('0');
   for(size_t i = 0; input.length() > i; ++i)
-	{
-		ss << setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(input[i]));
+  {
+    ss << setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(input[i]));
   }
-
+  
   return ss.str(); 
 }
 
 string Hexadecimal::decode(const string& input)
 {
-	string output;
-
+  string output;
+  
   if((input.length() % 2) != 0)
-	{
-  	throw runtime_error("String is not valid length ...");
+  {
+    throw runtime_error("String is not valid length ...");
   }
-
-	size_t cnt = input.length() / 2;
-
+  
+  size_t cnt = input.length() / 2;
+  
   for(size_t i = 0; cnt > i; ++i)
-	{
-		uint32_t s = 0;
+  {
+    uint32_t s = 0;
     stringstream ss;
     ss << hex << input.substr(i * 2, 2);
     ss >> s;
-
     output.push_back(static_cast<unsigned char>(s));
   }
-
+  
   return output;
 }
 
