daf25369f160a37af8bd547299c02ef90da99d71
cedi <cedi@users.noreply.github.com>
2017-10-08 18:49:22 +0200

    prepare FrequencyCounter for more features

--- libcryptopals/include/FrequencyCounter.hpp
+++ libcryptopals/include/FrequencyCounter.hpp
@@ -6,17 +6,26 @@
 class CharFreqCntr
 {
 public:
-  CharFreqCntr(std::string input, int charWidth);
+	CharFreqCntr(std::string input, int charWidth);
 
-  std::map<std::string, double> getSingleCharFreq();
-  std::map<std::string, double> getDualCharFreq();
-  std::map<std::string, double> getRepeatingChars();
+	std::map<std::string, double> getSingleCharFreq(bool force=false);
+	std::map<std::string, double> getDualCharFreq(bool force=false);
+	std::map<std::string, double> getRepeatingChars(bool force=false);
+
+protected:
+	void analyzeSinglecharFreq();
+	void analyzeDualCharFreq();
+	void analyzeRepeatingChars();
 
 private:
-  std::string getChar(int offset);
+	std::string getChar(int offset);
 
 // Member Variables
 private:
-  std::string input;
-  int charWidth;
+	std::string input;
+	int charWidth;
+
+	std::map<std::string, double> singleCharFreq;
+	std::map<std::string, double> dualCharFreq;
+	std::map<std::string, double> repeatingCharFreq;
 };
--- libcryptopals/src/FrequencyCounter.cpp
+++ libcryptopals/src/FrequencyCounter.cpp
@@ -6,96 +6,127 @@
 using namespace std;
 
 CharFreqCntr::CharFreqCntr(std::string input, int charWidth)
-  : input(input)
-  , charWidth(charWidth)
+	: input(input)
+	, charWidth(charWidth)
 {
 }
 
-map<string, double> CharFreqCntr::getSingleCharFreq()
+map<string, double> CharFreqCntr::getSingleCharFreq(bool force)
 {
-  if((input.size() % 2) != 0)
-  {
-    throw invalid_argument("StringLen not correct padded");
-  }
+	if(force || singleCharFreq.empty())
+	{
+		analyzeSinglecharFreq();
+	}
 
-  map<string, double> occurrences;
-  for(int i = 0; i < input.size(); i += charWidth)
-  {
-    string myChar = getChar(i);
-    
-    auto ret = occurrences.insert(make_pair(myChar, 1));
-    if(ret.second == false)
-    {
-      int count = ret.first->second;
-      count++;
-      ret.first->second = count;
-    }
-  }
-
-  return occurrences;
+	return singleCharFreq;
 }
 
-map<string, double> CharFreqCntr::getDualCharFreq()
+map<string, double> CharFreqCntr::getDualCharFreq(bool force)
 {
-  if((input.size() % 2) != 0)
-  {
-    throw invalid_argument("StringLen not correct padded");
-  }
+	if(force || dualCharFreq.empty())
+	{
+		analyzeDualCharFreq();
+	}
 
-  map<string, double> occurrences;
-  for(int i = 0; i < input.size(); i += charWidth * 2)
-  {
-    string myChar;
-    {
-      string myChar1 = getChar(i);
-      string myChar2 = getChar(i + charWidth);
-      myChar = myChar1 + myChar2;
-    }
-    
-    auto ret = occurrences.insert(make_pair(myChar, 1));
-    if(ret.second == false)
-    {
-      int count = ret.first->second;
-      count++;
-      ret.first->second = count;
-    }
-  }
-
-  return occurrences;
+	return dualCharFreq;
 }
 
-map<string, double> CharFreqCntr::getRepeatingChars()
+map<string, double> CharFreqCntr::getRepeatingChars(bool force)
 {
-  if((input.size() % 4) != 0)
-  {
-    throw invalid_argument("StringLen not correct padded");
-  }
+	if(force || repeatingCharFreq.empty())
+	{
+		analyzeRepeatingChars();
+	}
 
-  map<string, double> occurrences;
-  for(int i = 0; i < input.size(); i += charWidth)
-  {
-    string myChar1 = getChar(i);
-    string myChar2 = getChar(i + charWidth);
+	return repeatingCharFreq;
+}
 
-    if(myChar1 != myChar2)
-    {
-      continue;
-    }
-    
-    auto ret = occurrences.insert(make_pair(myChar1, 1));
-    if(ret.second == false)
-    {
-      int count = ret.first->second;
-      count++;
-      ret.first->second = count;
-    }
-  }
 
-  return occurrences;
+void CharFreqCntr::analyzeSinglecharFreq()
+{
+	if((input.size() % 2) != 0)
+	{
+		throw invalid_argument("StringLen not correct padded");
+	}
+
+	map<string, double> occurrences;
+	for(int i = 0; i < input.size(); i += charWidth)
+	{
+		string myChar = getChar(i);
+		
+		auto ret = occurrences.insert(make_pair(myChar, 1));
+		if(ret.second == false)
+		{
+			int count = ret.first->second;
+			count++;
+			ret.first->second = count;
+		}
+	}
+
+	singleCharFreq = occurrences;
+}
+
+void CharFreqCntr::analyzeDualCharFreq()
+{
+	if((input.size() % 2) != 0)
+	{
+		throw invalid_argument("StringLen not correct padded");
+	}
+
+	map<string, double> occurrences;
+	for(int i = 0; i < input.size(); i += charWidth * 2)
+	{
+		string myChar;
+		{
+			string myChar1 = getChar(i);
+			string myChar2 = getChar(i + charWidth);
+			myChar = myChar1 + myChar2;
+		}
+		
+		auto ret = occurrences.insert(make_pair(myChar, 1));
+		if(ret.second == false)
+		{
+			int count = ret.first->second;
+			count++;
+			ret.first->second = count;
+		}
+	}
+
+	dualCharFreq = occurrences;
+}
+
+void CharFreqCntr::analyzeRepeatingChars()
+{
+	if((input.size() % 4) != 0)
+	{
+		throw invalid_argument("StringLen not correct padded");
+	}
+
+	map<string, double> occurrences;
+	for(int i = 0; i < input.size(); i += charWidth)
+	{
+		string myChar1 = getChar(i);
+		string myChar2 = getChar(i + charWidth);
+
+		if(myChar1 != myChar2)
+		{
+			continue;
+		}
+		
+		auto ret = occurrences.insert(make_pair(myChar1, 1));
+		if(ret.second == false)
+		{
+			int count = ret.first->second;
+			count++;
+			ret.first->second = count;
+		}
+	}
+
+	repeatingCharFreq = occurrences;
 }
 
 
 string CharFreqCntr::getChar(int offset)
 {
-  return string(input, offset, charWidth);
+	return string(input, offset, charWidth);
 }
