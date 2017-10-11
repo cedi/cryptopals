eaf79c93ef52e2baf550b90c956a942a8456af61
cedi <cedi@users.noreply.github.com>
2017-10-11 21:05:33 +0200

    read multiple strings from a file and crack them
    at the end, only the crack with the best score will win

--- Set1/Challenge4/main.cpp
+++ Set1/Challenge4/main.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <tuple>
 #include <list>
+#include <fstream>
 
 #include "Hexadecimal.hpp"
 #include "XOR.hpp"
@@ -71,28 +72,68 @@ tuple<string, string, double> getBestMatch(const list<tuple<string, string, doub
 	{
 		if(get<2>(bestMatch) < get<2>(result))
 		{
-			bestMatch = make_tuple(get<0>(result), get<1>(result), get<2>(result));
+			bestMatch = result;
 		}
 	}
 
 	return bestMatch;
 }
 
-int main()
+tuple<string, string, string, double> getBestMatch(const list<tuple<string, string, string, double>>& cryptedKeyDecryptedNScored)
 {
-	string crypted = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+	tuple<string, string, string, double> bestMatch;
 
+	for(auto result : cryptedKeyDecryptedNScored)
+	{
+		if(get<2>(bestMatch) < get<2>(result))
+		{
+			bestMatch = result;
+		}
+	}
+
+	return bestMatch;
+}
+
+tuple<string, string, double> crack(string crypted)
+{
 	map<string, double> possibleKeys;
 	probePossibleKeys(crypted, &possibleKeys);
 
 	list<tuple<string, string, double>> decryptedNScored;
 	decryptAndScore(crypted, possibleKeys, &decryptedNScored);
 
-	tuple<string, string, double> bestMatch = getBestMatch(decryptedNScored);
+	auto bestMatch = getBestMatch(decryptedNScored);
+	cout << "Decrypted: " << get<1>(bestMatch) << endl;
 
-	cout << "Key is:    '" << get<0>(bestMatch) << "'" << endl;
-	cout << "Encrypted: '" << crypted << "'" << endl;
-	cout << "Decrypted: '" << get<1>(bestMatch) << "'" << endl;
+	return bestMatch;
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		cerr << "Usage: ./programm filename" << endl;
+		return 1;
+	}
+
+	ifstream infile(argv[1]);
+
+	list<tuple<string, string, string, double>> bestMatches;
+
+	string crypted;
+	while(getline(infile, crypted))
+	{
+		cout << "Try: " << crypted << endl;
+		auto cracked = crack(crypted);
+		bestMatches.push_back(make_tuple(crypted, get<0>(cracked), get<1>(cracked), get<2>(cracked)));
+	}
+	
+	auto bestMatch = getBestMatch(bestMatches);
+
+	cout << "Encrypted String: '" << get<0>(bestMatch) << "'" << endl;
+	cout << "Key:              '" << get<1>(bestMatch) << "'" << endl;
+	cout << "Decrypted:        '" << get<2>(bestMatch) << "'" << endl;
+
 	return 0;
 }
 
