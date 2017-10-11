44cd5d194a76fbf20e60ba6c09a3883f1d312e2a
cedi <cedi@users.noreply.github.com>
2017-10-11 20:21:22 +0200

    some cleanup

--- Set1/Challenge4/main.cpp
+++ Set1/Challenge4/main.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <sstream>
 #include <cstring>
+#include <tuple>
+#include <list>
 
 #include "Hexadecimal.hpp"
 #include "XOR.hpp"
@@ -11,37 +13,15 @@
 
 using namespace std;
 
-void printKV(const map<string, double> occurrence)
-{
-	for(auto x : occurrence)
-	{
-		cout << x.first << ": " << x.second << endl;
-	}
-}
 
-int main()
+void probePossibleKeys(const string& crypted, map<string, double>* possibleKeys)
 {
-	string crypted = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 	CharFreqCntr freq(crypted, 2);
 
 	auto singleChar = freq.getFrequency(CharFreqCntr::SINGLE);
 
-#if 0
-	cout << "Single Character Occurrences:" << endl;
-	printKV(singleChar);
-	
-	auto dualChar = freq.getFrequency(CharFreqCntr::DUAL);
-	cout << "Dual Character Occurrences:" << endl;
-	printKV(dualChar);
-
-	auto repeatingChar = freq.getFrequency(CharFreqCntr::REPEATING);
-	cout << "Repeating Chars:" << endl;
-	printKV(repeatingChar);
-#endif
-
 	cout << "Try to break it" << endl;
 	const string mostCurrentPossibilities = "ETAOIN SHRDLU";
-	map<string, double> possibleKeys;
 
 	// get possible key as character by probing every ASCII character
 	// This means try to XOR every ASCII character with one of the possible
@@ -56,16 +36,25 @@ int main()
 			string encrypted = Hexadecimal::encode(XOR::encode(string(1, possibleCurrentChar), toProbe));
 			if(encrypted == freq.getMostOccurent(CharFreqCntr::SINGLE))
 			{
-				//cout << "Key seems to be: '" << toProbe << "'" << endl;
-				possibleKeys.insert(make_pair(toProbe, 0));
-				break;
+				possibleKeys->insert(make_pair(toProbe, 0));
 			}
 		}
 	}
 
+}
+
+int main()
+{
+	string crypted = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+	map<string, double> possibleKeys;
+	probePossibleKeys(crypted, &possibleKeys);
+	
 	// Now try to decrypt the encrypted text using the above calculated keys
 	// and analyze every result using the TextScoring class
 	string chipertext = Hexadecimal::decode(crypted);
+
+	list<tuple<string, string, double>> decryptedNScored;
 
 	for(auto iter : possibleKeys)
 	{
@@ -78,49 +67,25 @@ int main()
 		string decoded = XOR::encode(chipertext, key);
 		TextScoring score(Hexadecimal::encode(decoded));
 
-		possibleKeys[character] = score.analyze(TextScoring::PRINTABLE, TextScoring::DECODE_HEX);
-
-		//cout << "Trying key: '" << key << "'" << endl;
+		decryptedNScored.push_back(make_tuple(key, decoded, score.analyze(TextScoring::PRINTABLE, TextScoring::DECODE_HEX)));
 	}
 
 	double score = 0;
-	string character;
-	for(auto iter : possibleKeys)
+	string key;
+	string decrypted;
+	for(auto result : decryptedNScored)
 	{
-		if(score < iter.second)
+		if(score < get<2>(result))
 		{
-			score = iter.second;
-			character = iter.first;
+			key = get<0>(result);
+			decrypted = get<1>(result);
+			score = get<2>(result);
 		}
 	}
 
-	cout << "Key is: '" << character << "'" << endl;
-
-	char* key = (char*)malloc(chipertext.size() + 1);
-	memset(key, character.c_str()[0], chipertext.size());
-	key[chipertext.size()] = '\0';
-
-	string decoded = XOR::encode(chipertext, key);
-
-	cout << "Encrypted:	" << chipertext.size() << ": '" << chipertext << "'" << endl;
-	cout << "Decrypted:	" << decoded.size() << ": '" << decoded << "'" << endl;
+	cout << "Key is:    '" << key << "'" << endl;
+	cout << "Encrypted: '" << chipertext << "'" << endl;
+	cout << "Decrypted: '" << decrypted << "'" << endl;
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
