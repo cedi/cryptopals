e88d38653cf3e4cc365c762ccd8743c90418ece7
cedi <cedi@users.noreply.github.com>
2017-11-01 18:49:20 +0100

    finish the fucking level...
    The only problem was the missunderstanding of the input/output
    strings...
    Everything works perfectly...
    Fuck everything... This was way to easy to fix... I hate it so much,
    when i think this is going to be a more serious problem and then it's
    just a missunderstanding of the IV...

--- Set1/Challenge5/main.cpp
+++ Set1/Challenge5/main.cpp
@@ -12,7 +12,7 @@ int main()
 	cout << "*        Repeating key XOR        *" << endl;
 	cout << "***********************************" << endl << endl;
 
-	string line1 = "Burning 'em, if you ain't quick and nimble";
+	string line1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 	
 	string key = "ICE";
 
@@ -21,14 +21,5 @@ int main()
 	cout << "Is crypted:  ";
 	cout << crypted1 <<  endl;
 	cout << "Should be:   ";
-	cout << "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" << endl;
-
-	cout << endl;
-
-	//string line2 = "I go crazy when I hear a cymbal";
-	//string crypted2 = XOR::encode(Utils::strToByteVec(line2), Utils::strToByteVec(key));
-	//cout << "Is crypted:  ";
-	//cout << crypted2 <<  endl;
-	//cout << "Should be:   ";
-	//cout << "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" << endl;
+	cout << "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" << endl;
 }
