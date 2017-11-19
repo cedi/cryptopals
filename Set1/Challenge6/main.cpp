0a08fa71084dd16d455392cf398bd385654a742c
cedi <cedi@users.noreply.github.com>
2017-11-19 17:07:08 +0100

    create challenge6

---
+++ Set1/Challenge6/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Hexadecimal.hpp"
+#include "XOR.hpp"
+#include "Utils.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "*****************************************" << endl;
+	cout << "*        Break Repeating key XOR        *" << endl;
+	cout << "*****************************************" << endl << endl;
+}
